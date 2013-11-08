042fe6c4a3e89090e9444ed9686d4fe39e16037f
pelukron <diegohmore@hotmail.com>
2013-11-08 01:13:58 -0600

    Create nodo.java

---
+++ nodo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista;
+
+/**
+ *
+ * @author Diego M.
+ */
+public class nodo {
+    public long dato;
+    public nodo liga;
+    public nodo(long dato){
+        this.dato=dato;
+        
+    }
+    
+}
